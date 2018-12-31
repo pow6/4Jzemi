e5abd5257215d321a1c1b9f29d7b20578094245d
pow6 <fake.existences@gmail.com>
2018-12-31 19:42:32 +0900

    add button of initial activity

--- GC/Android/socketTrans/app/src/main/java/net/pow6/sockettrans/InitialActivity.java
+++ GC/Android/socketTrans/app/src/main/java/net/pow6/sockettrans/InitialActivity.java
@@ -1,24 +1,31 @@
 package net.pow6.sockettrans;
 
 import android.content.Intent;
+import android.hardware.SensorManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class InitialActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class InitialActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        findViewById(R.id.button_start).setOnClickListener(this);
+        findViewById(R.id.button_setting).setOnClickListener(this);
+        findViewById(R.id.button_info).setOnClickListener(this);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-
         //wifi on/off の取得
         WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
         if(wifiManager.isWifiEnabled() == false){
@@ -33,6 +40,30 @@ public class InitialActivity extends AppCompatActivity {
         return true;
     }
 
+    //ボタンを押したときの処理
+    @Override
+    public void onClick(View view){
+        if(view != null){
+            Intent intent;
+            switch(view.getId()){
+                case R.id.button_start:
+                    intent = new Intent(InitialActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.button_setting:
+                    intent = new Intent(InitialActivity.this,SettingActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.button_info:
+                    intent = new Intent(InitialActivity.this,InfoActivity.class);
+                    startActivity(intent);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
 
     //メニュー選択時の処理
     @Override
